88d25fbce8ec433cf72f3cff1f84e0816c1008e6
madepores <martin.barindirimana@aims.ac.rw>
2023-09-12 10:53:40 -0700

    m

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
 	    int r;
-
 
 	    r = print_sign(98);
 	    _putchar(',');
