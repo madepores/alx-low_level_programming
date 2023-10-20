bc149a4e74b3f25735d443ca4c23671b3933207d
madepores <martin.barindirimana@aims.ac.rw>
2023-10-20 14:13:41 +0200

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 
+/**
+* print_list - check the code for Holberton School students.
+* @h: name of the list
+* Return: the number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
- 
-size_t cons = 0;
+   int count = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- cons = h->next;
- cons++;
- }
 
- return (cons);
+   while (h)
+   {
+       if (h->str == NULL)
+       {
+           printf("[0] (nil)\n");
+       }
+       else
+       {
+       printf("[%d] %s\n", h->len, h->str);
+       }
+       count++;
+       h = h->next;
+   }
+   return (count);
 }
