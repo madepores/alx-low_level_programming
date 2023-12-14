6bf0a0c2db3a15b163cced80ec6e95097925c7ff
madepores <martin.barindirimana@aims.ac.rw>
2023-12-14 07:23:19 -0800

    l

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/madepores/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libhint.so
+wget -P .. https://raw.githubusercontent.com/madepores/alx-low_level_programming/main/0x18-dynamic_libraries/libhint.so
 export LD_PRELOAD="$PWD/../libhint.so"
